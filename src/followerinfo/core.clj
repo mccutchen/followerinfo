4e3a88e0bdb0b23bcc2e6d05538800a7e2a09768
Will McCutchen <wm@bitly.com>
2012-08-07 14:36:54 -0400

    First working version (for a single account).

--- src/followerinfo/core.clj
+++ src/followerinfo/core.clj
@@ -1,7 +1,3 @@
-;; (ns followerinfo.core
-;;   (:use clojure.tools.cli)
-;;   (:gen-class))
-
 (ns followerinfo.core
   (:require [cheshire.core :as json]
             [clojure.tools.cli :as cli]
@@ -12,24 +8,51 @@
 (def twitter-api-base "http://api.twitter.com/1")
 
 (defn twitter-api
+  "Makes a call to the Twitter API and returns the decoded response body."
   [endpoint params]
+  (log/infof "Twitter API: %s %s" endpoint params)
   (let [url (str twitter-api-base endpoint)
         resp (client/get url {:query-params params :as :json})]
     (:body resp)))
 
 (defn fetch-follower-ids
-  "Returns a list of user IDs of the followers of the given Twitter handle"
+  "Returns a lazy seq of user IDs of the followers of the given user."
   ([handle]
      (fetch-follower-ids handle -1 []))
   ([handle cursor acc]
-     (printf "Fetching followers of %s (cursor: %s; count: %d)\n"
-             handle cursor (count acc))
      (let [resp (twitter-api "/followers/ids.json" {:screen_name handle :cursor cursor})
            ids (:ids resp)
            next_cursor (:next_cursor resp)]
        (if (> next_cursor 0)
          (recur handle next_cursor (concat acc ids))
          (concat acc ids)))))
+
+(defn fetch-follower-infos
+  "Returns a lazy seq of maps of user info for the given IDs."
+  ([ids]
+     (fetch-follower-infos ids []))
+  ([ids acc]
+     (let [these-ids (take 100 ids)
+           rest-ids (drop 100 ids)
+           infos (twitter-api "/users/lookup.json" {:user_id (apply str (interpose "," these-ids))})]
+       (if (empty? rest-ids)
+         (concat acc infos)
+         (recur rest-ids (concat acc infos))))))
+
+(defn fetch-followers
+  "Returns a lazy seq of maps of infomation about each of the followers of the
+  given Twitter handle."
+  [handle]
+  (let [ids (fetch-follower-ids handle)]
+    (fetch-follower-infos ids)))
+
+(defn make-formatter
+  "Returns a function suitable for formatting each follower info record
+  according to whether we want verbose output."
+  [verbose?]
+  (if verbose?
+    (fn [rec] (println (json/encode rec)))
+    (fn [{:keys [screen_name id]}] (printf "%s,%s\n" screen_name id))))
 
 (defn -main
   [& args]
@@ -42,6 +65,9 @@
       (println banner)
       (System/exit 0))
     (let [handle (first args)
-          follower-ids (fetch-follower-ids handle)]
-      (println)
-      (printf "@%s has %d followers\n" handle (count follower-ids)))))
+          followers (fetch-followers handle)
+          formatter (make-formatter (:verbose options))]
+      (log/infof "@%s has %d followers\n" handle (count followers))
+      ;; need to use doseq because followers is a lazy seq and map wouldn't
+      ;; realize it.
+      (doseq [rec followers] (formatter rec)))))
